2f605f8e41b7fd53ddc5f4e1d9098c485f8d74bc
JuanTomey <jumatogo.yumatogo@gmail.com>
2025-07-23 21:31:04 -0500

    Actualizacion de views

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import CentroComercial, Tienda
+from .models import CentroComercial, Tienda, Archivo
 
 # Create your views here.
 def lista_centros_comerciales(request):
